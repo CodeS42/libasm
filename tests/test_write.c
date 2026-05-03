3b5fc69a501331f5b4d4548455359fbcb5f547b9
Serena Gandum--Diagne <sgandum-@f2r4s11.clusters.42paris.fr>
2026-05-03 15:07:58 +0200

    write main test done

--- tests/test_write.c
+++ tests/test_write.c
@@ -1,27 +1,47 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
 extern ssize_t ft_write(int fd, const void *buf, size_t count);
+
+void results(char *s)
+{
+    ssize_t ret_ft_write;
+    ssize_t ret_write;
+    int err_ft_write;
+    int err_write;
+
+    ret_ft_write = 0;
+    ret_write = 0;
+    errno = 0;
+
+    write(1, "ft_write() result: ", 19);
+    ret_ft_write = ft_write(1, s, 12);
+    err_ft_write = errno;
+    errno = 0;
+    write(1, "\nwrite() result: ", 17);
+    ret_write = write(1, s, 12);
+    err_write = errno;
+
+    printf("\nft_write() returns: %zd\n", ret_ft_write);
+    printf("write() returns: %zd\n", ret_write);
+
+    printf("ft_write() errno: %d (%s)\n", err_ft_write, strerror(err_ft_write));
+    printf("write() errno: %d (%s)\n", err_write, strerror(err_write));
+}
 
 int main()
 {
     char *s;
-    ssize_t result_ft_write;
-    ssize_t result_write;
-
+    
     s = "Hello World!";
-    result_ft_write = 0;
-    result_write = 0;
-
     printf("- With s = 'Hello World!' -\n");
-    write(1, "ft_write() result: ", 19);
-    result_ft_write = ft_write(1, s, 12);
-    write(1, "\nwrite() result: ", 17);
-    result_write = write(1, s, 12);
+    results(s);
 
-    printf("\nft_write() returns: %zu\n", result_ft_write);
-    printf("write() returns: %zu\n", result_write);
+    s = NULL;
+    printf("\n- With s = NULL -\n");
+    results(s);
 
     return (0);
 }
-// tests avec NULL etc
